a7fe39aa08afbacbc60a282490759832792a6af2
Tiago Generoso <tiagogeneroso47@gmail.com>
2025-01-29 15:36:32 -0300

    removed time

--- utils/jwtUtils.js
+++ utils/jwtUtils.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const JWT_SECRET = process.env.JWT_SECRET || 'VidaNova2025';
 
 const generateToken = (userId, isAdmin, name, phone, email) => {
-  return jwt.sign({ userId, isAdmin, name, phone, email }, JWT_SECRET, { expiresIn: '1h' });
+  return jwt.sign({ userId, isAdmin, name, phone, email }, JWT_SECRET);
 };
 
 const verifyToken = (token) => {
