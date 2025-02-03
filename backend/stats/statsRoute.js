f0e446538500f184a304f925c184f642e9b2600c
Tiago Generoso <tiagogeneroso47@gmail.com>
2025-02-03 14:58:37 -0300

    prontop

--- backend/stats/statsRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getStats } = require('../stats/statsController'); 
-const authMiddleware = require('../backend/middleware/authMiddleware');
-
-router.get('/', authMiddleware, getStats); 
-
-module.exports = router;
--- backend/users/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { showUser } = require('../users/userController');
-const authMiddleware = require('../backend/middleware/authMiddleware'); 
-
-router.get('/:id', authMiddleware, showUser);
-
-module.exports = router;
