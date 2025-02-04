fd73bc17f1bd6abda9472e9234c0bf6f4223d7f9
Tiago Generoso <tiagogeneroso47@gmail.com>
2025-02-04 09:44:16 -0300

    .env

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { showUser } = require('../../controllers/userController');
-const authMiddleware = require('../../middleware/authMiddleware'); 
+const { showUser } = require('../controllers/userController');
+const authMiddleware = require('../middleware/authMiddleware'); 
 
 router.get('/:id', authMiddleware, showUser);
 
