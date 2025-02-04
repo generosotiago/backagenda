7a8f0da2304d52d569bf405f9b39fe566a31d61a
Tiago Generoso <tiagogeneroso47@gmail.com>
2025-02-04 09:27:27 -0300

    not working

--- users/routes/roomRoute.js
+++ users/routes/roomRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const roomController = require('../controllers/roomController');
+const roomController = require('../../controllers/roomController');
 
 router.post('/', roomController.createRoom);
 router.get('/', roomController.getRooms);
